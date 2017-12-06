e3d1f100b7d1548c6b390002328c1191f8898f81
errornosignal <errornosignal@users.noreply.github.com>
2017-12-06 00:21:46 -0600

    final

--- Apple_HW3_ReverseDigit/main.swift
+++ Apple_HW3_ReverseDigit/main.swift
@@ -8,5 +8,75 @@
 
 import Foundation
 
-print("nothing to see here.")
+// print program header
+print("Apple_HW3_ReverseDigit")
+print("Type '0' to exit")
 
+var originalInput = ""
+
+//loop for geting int from user
+func validateIntInput(prompt: String) -> Int {
+    while (true) {
+        print(prompt);
+        originalInput = readLine()!
+        let inputInt = Int(originalInput)
+        
+        //allow exit
+        if (inputInt == 0) {
+            exit(5)
+        }
+        //check for valid input
+        else if (inputInt != nil ) {
+            if (inputInt! < 0 || inputInt! > 0) {
+                return inputInt!;
+            }
+        }
+        //display error for bad input
+        else {
+            print("Error! Invalid input. Value must be an integer.");
+        }
+    }
+}
+
+//reverse input and strip off zeroes
+func reverse(value: Int) -> Int {
+    var newNumber = value
+    var reversedNumber = 0
+    let divisor = 10
+    var dividedNumber = 0
+    
+    reversedNumber = (reversedNumber * divisor) + dividedNumber;
+    
+    while (newNumber < 0 || newNumber > 0) {
+        dividedNumber = newNumber % divisor;
+        reversedNumber = (reversedNumber * divisor) + dividedNumber;
+        newNumber /= divisor;
+    }
+    return reversedNumber;
+}
+
+//reverse the reversed input from the previous function and discard sign of value
+func reReverse (value: Int) -> Int {
+    var newNumber = value
+    if (newNumber < 0) {
+        newNumber = newNumber * -1
+    }else {/*doNothing()*/}
+    return reverse(value: newNumber)
+}
+
+//main
+while(true) {
+    print()
+    
+    //get user input
+    let inputInt = validateIntInput(prompt: "Enter a whole number: ")
+    
+    //run numbers through functions
+    let reversedNumber = reverse(value: inputInt)
+    let reReversedNumber = reReverse(value: reversedNumber)
+
+    //output to console
+    print("originalInput = " + originalInput)
+    print("reversedNumberString = " + String(reversedNumber))
+    print("reReversedNumberString = " + String(reReversedNumber))
+}
